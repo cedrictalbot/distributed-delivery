6a305855149c7dcf3d18c300859f186b897e485a
Cédric Talbot <cedric.talbot@student.ecp.fr>
2020-03-25 19:01:38 +0100

    Add maximum throughput to Capacity offload graph

--- src/components/CapacityOffloadGraph.js
+++ src/components/CapacityOffloadGraph.js
@@ -7,22 +7,29 @@ export default class CapacityOffloadGraph extends React.Component {
         super(props);
 
         this.state = {
-            chartData : null
+            chartData : null,
+            maxCdnP2p : null
         }
     }
 
     getChartData() {
         const {data} = this.props;
         var chartData = [];
+        var maxCdnP2p = 0;
         for (var i in data.cdn) {
             // We suppose for now that cdn and p2p have the same timestamps in the same order
+            var cdn = data.cdn[i][1]/(10**9);
+            var p2p = data.p2p[i][1]/(10**9);
+            if (cdn + p2p > maxCdnP2p) {
+                maxCdnP2p = cdn + p2p;
+            }
             chartData.push({
                 'date': data.cdn[i][0],
-                'cdn' : data.cdn[i][1]/(10**9),
-                'p2p' : data.p2p[i][1]/(10**9)
+                'cdn' : cdn,
+                'p2p' : p2p
             })
         }
-        this.setState({chartData})
+        this.setState({chartData, maxCdnP2p})
 
     }
 
@@ -49,6 +56,7 @@ export default class CapacityOffloadGraph extends React.Component {
     render() {
         const ticks = this.getTicks();
         const maxCdn = (this.props.maxCdn/ 10**9).toFixed(2);
+        const maxCdnP2p = this.state.maxCdnP2p ? this.state.maxCdnP2p.toFixed(2) : 0;
         const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         return (
             <AreaChart width={1000} height={300} data={this.state.chartData}>
@@ -65,6 +73,7 @@ export default class CapacityOffloadGraph extends React.Component {
                 <Area type='monotone' dataKey='cdn' stackId="1" stroke='#C42151' fill='#C42151' />
                 <Area type='monotone' dataKey='p2p' stackId="1" stroke='#12A5ED' fill='#12A5ED' />
                 <ReferenceLine y={maxCdn} label={{value :`Maximum CDN contribution : ${maxCdn} Gbps`, position: 'insideBottomLeft'}} stroke='#C42151' strokeDasharray="5 2"/>
+                <ReferenceLine y={maxCdnP2p} label={{value :`Maximum throughput : ${maxCdnP2p} Gbps`, position: 'insideBottomRight'}} stroke='#3FCB7E' strokeDasharray="5 2"/>
             </AreaChart>
         )
     }

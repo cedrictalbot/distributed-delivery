afda3cbe0ccbccb1228a4a438b226c4efbf23051
Cédric Talbot <cedric.talbot@student.ecp.fr>
2020-03-25 18:29:43 +0100

    Set X axis of Capacity Offload Graph to get a value per day

--- src/components/CapacityOffloadGraph.js
+++ src/components/CapacityOffloadGraph.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {AreaChart, XAxis, YAxis, Tooltip, Area} from 'recharts';
 
+
 export default class CapacityOffloadGraph extends React.Component {
     constructor(props) {
         super(props);
@@ -16,7 +17,7 @@ export default class CapacityOffloadGraph extends React.Component {
         for (var i in data.cdn) {
             // We suppose for now that cdn and p2p have the same timestamps in the same order
             chartData.push({
-                'date': new Date(data.cdn[i][0]),
+                'date': data.cdn[i][0],
                 'cdn' : data.cdn[i][1]/(10**9),
                 'p2p' : data.p2p[i][1]/(10**9)
             })
@@ -28,10 +29,36 @@ export default class CapacityOffloadGraph extends React.Component {
     componentDidMount() {
         this.getChartData();
     }
+
+    getTicks() {
+        if (!this.state.chartData) {
+            return []
+        }
+        var ticks = [];
+        var d = new Date(this.state.chartData[0].date);
+        const endDate = new Date(this.state.chartData[this.state.chartData.length - 1].date)
+        // We suppose here timestamps are regular enough there will be a data everyday at the same time
+        // Another option would be to take the closest timestamp to 12AM
+        while (d < endDate) {
+            ticks.push(d.getTime());
+            d.setDate(d.getDate() + 1);
+        }
+        return ticks;
+    }
+
     render() {
+        const ticks = this.getTicks();
+        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         return (
             <AreaChart width={1000} height={300} data={this.state.chartData}>
-                <XAxis dataKey="date"/>
+                <XAxis 
+                    dataKey="date" 
+                    tickFormatter={(label) =>{
+                        const d = new Date(label);
+                        return(`${d.getDate()} ${months[d.getMonth()]}`)
+                    }}
+                    ticks = {ticks}
+                />
                 <YAxis tickFormatter={(label) => label ? `${label}\nGbps` : label}/>
                 <Tooltip />
                 <Area type='monotone' dataKey='cdn' stackId="1" stroke='#C42151' fill='#C42151' />

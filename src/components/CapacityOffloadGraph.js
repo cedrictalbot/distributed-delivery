b25337542e13ba4a3f4a0a1a91f673bd0b458717
Cédric Talbot <cedric.talbot@student.ecp.fr>
2020-03-25 16:25:28 +0100

    Build capacity offload graph

--- src/components/CapacityOffloadGraph.js
+++ src/components/CapacityOffloadGraph.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import {AreaChart, XAxis, YAxis, Tooltip, Area} from 'recharts';
 
 export default class CapacityOffloadGraph extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            chartData : null
+        }
+    }
+
+    getChartData() {
+        const {data} = this.props;
+        var chartData = [];
+        for (var i in data.cdn) {
+            // We suppose for now that cdn and p2p have the same timestamps in the same order
+            chartData.push({
+                'date': new Date(data.cdn[i][0]),
+                'cdn' : data.cdn[i][1],
+                'p2p' : data.p2p[i][1]
+            })
+        }
+        this.setState({chartData})
+
+    }
+
+    componentDidMount() {
+        this.getChartData();
+    }
     render() {
-        return null
-
+        return (
+            <AreaChart width={1000} height={300} data={this.state.chartData}>
+                <XAxis dataKey="date"/>
+                <YAxis/>
+                <Tooltip />
+                <Area type='monotone' dataKey='cdn' stackId="1" stroke='#C42151' fill='#C42151' />
+                <Area type='monotone' dataKey='p2p' stackId="1" stroke='#12A5ED' fill='#12A5ED' />
+            </AreaChart>
+        )
     }
 }

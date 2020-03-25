902db83d95843433121e0bc450780ff4e1ee71ca
Cédric Talbot <cedric.talbot@student.ecp.fr>
2020-03-25 14:04:02 +0100

    Import data from the backend using static values & store the session token in a cookie

--- src/components/GraphContainer.js
+++ src/components/GraphContainer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import cookie from 'react-cookies';
 
 import CapacityOffloadGraph from './CapacityOffloadGraph';
 import ConcurrentViewersGraph from './ConcurrentViewersGraph';
@@ -6,19 +8,90 @@ import TimelineGraph from './TimelineGraph';
 
 
 export default class GraphContainer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dataLoaded : false,
+            bandwidthData : null,
+            audienceData : null,
+            sessionToken : cookie.load("sessionToken")
+        }
+    }
+
+    componentDidMount() {
+        if (!this.dataLoaded) {
+            if (!this.state.sessionToken) {
+                axios.post("http://localhost:3000/auth", {
+                    "identifiant": "urtoob",
+                    "password": "ToobRU"
+                }).then(res => {
+                    const sessionToken = res.data.session_token;
+                    cookie.save("sessionToken", sessionToken);
+                    this.setState({sessionToken});
+                    this.getData();
+                }).catch(err => {
+                    // handle error
+                    console.log(err);
+                });
+            }
+            else {
+                this.getData();
+            }
+        }
+    }
+
+    async getData(){
+        const today = Date.now();
+        let data = {
+            bandwidth : null,
+            audience : null,
+        }
+        try {
+            data.bandwidth = await axios.post("http://localhost:3000/bandwidth", {
+                "session_token" : this.state.sessionToken,
+                "from" : today - 1209600000,
+                "to" : today
+            });
+            data.audience = await axios.post("http://localhost:3000/audience", {
+                "session_token" : this.state.sessionToken,
+                "from" : today - 1209600000,
+                "to" : today
+            });
+        } catch (err) {
+            console.log(err);
+        }
+        this.setState({
+            dataLoaded: true,
+            bandwidthData : data.bandwidth.data,
+            audienceData : data.audience.data
+        })
+    }
+
+    componentWillUnmount() {
+        if (this.state.sessionToken) {
+            axios.post("http://localhost:3000/logout", {
+                "session_token" : this.state.sessionToken
+            })
+        }
+        cookie.remove("sessionToken")
+    }
+
     render() {
+        const {dataLoaded, bandwidthData, audienceData} = this.state;
         return (
-            <div>
+            dataLoaded &&
+            (<div>
                 <div>
-                    <CapacityOffloadGraph />
+                    <CapacityOffloadGraph data={bandwidthData}/>
                 </div>
                 <div>
-                    <ConcurrentViewersGraph />
+                    <ConcurrentViewersGraph data={audienceData}/>
                 </div>
                 <div>
                     <TimelineGraph />
                 </div>
-            </div>
+            </div>)
         )
     }
 }
